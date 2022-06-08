92d8eaeaaa2f5312645d2043e3ccd60f3e89ac43
Taylor Brown <81814258+tbrowntech@users.noreply.github.com>
2022-06-08 15:41:55 -0600

    Commit for changes made June 8, adding user inputs

--- budgister-app/src/components/ShoppingListPage.vue
+++ budgister-app/src/components/ShoppingListPage.vue
@@ -3,20 +3,20 @@
     <h3>Shopping List</h3>
     <div class="container">
       <ul>
-        <li contenteditable="true"></li>
-        <br />
-        <li contenteditable="true"></li>
-        <br />
-        <li contenteditable="true"></li>
-        <br />
-        <li contenteditable="true"></li>
-        <br />
-        <li contenteditable="true"></li>
-        <br />
-        <li contenteditable="true"></li>
-        <br />
-        <li contenteditable="true"></li>
+        <li v-for="item in items" :key="item.id">{{ item.label }}</li>
       </ul>
     </div>
   </section>
 </template>
+<script>
+export default {
+  data: () => ({
+    header: "Shopping List App",
+    items: [
+      { id: 1, label: "10 party hats" },
+      { id: 2, label: "2 board games" },
+      { id: 3, label: "20 cups" },
+    ],
+  }),
+};
+</script>
