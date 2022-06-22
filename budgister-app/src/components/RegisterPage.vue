de9fc6b4bc3035d1504b6b6f52c08e7d9a25bfec
Taylor Brown <ataylorbrown91@gmail.com>
2022-06-21 22:35:10 -0600

    update

--- budgister-app/src/components/RegisterPage.vue
+++ budgister-app/src/components/RegisterPage.vue
@@ -1,7 +1,21 @@
 <template>
   <section>
-    <div class="container">
-      <p>This is the register page</p>
+    <button id="edit">Edit</button>
+    <br />
+    <br />
+    <div class="grid-container" id="grid-title">
+      <div>Date</div>
+      <div>Posted</div>
+      <div>Description</div>
+      <div>Amount</div>
+      <div>Balance</div>
+    </div>
+    <div class="grid-container">
+      <div>6/13/22</div>
+      <div>Yes</div>
+      <div>Walmart</div>
+      <div>$52.00</div>
+      <div>$1592.00</div>
     </div>
   </section>
 </template>
